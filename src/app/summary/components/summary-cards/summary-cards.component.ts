4e803d7cef0ae1660ccecd95a98fc6f18b657474
Nery Caballero <ncaba@DESKTOP-THHI6CE>
2022-09-23 16:02:33 +0200

    Generated SummaryCards component. Added Markup and Style.

---
+++ src/app/summary/components/summary-cards/summary-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-cards',
+  templateUrl: './summary-cards.component.html',
+  styleUrls: ['./summary-cards.component.css']
+})
+export class SummaryCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
